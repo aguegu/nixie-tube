9905a5d867ffee4620989c105bcb2e532d313a7d
Weihong Guan <weihong.guan@gmail.com>
2012-05-04 16:19:58 +0800

    add copyright info

--- NixieTube/NixieTube.h
+++ NixieTube/NixieTube.h
@@ -7,6 +7,11 @@
  * Created on: 2012-5-4
  * Author: Weihong Guan (aGuegu) @ http://aguegu.net
  *
+ * Host at https://github.com/aguegu/nixie-tube
+ *
+ * licensed under Attribution-NonCommercial 3.0 Unported (CC BY-NC 3.0)
+ * http://creativecommons.org/licenses/by-nc/3.0/
+ *
  */
 
 #ifndef NIXIETUBE_H_

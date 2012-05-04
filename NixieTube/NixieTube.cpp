6fb33de926e1fed6ca7deefbd2e6e00179d26075
Weihong Guan <weihong.guan@gmail.com>
2012-05-04 16:28:51 +0800

    edit copyright info

--- NixieTube/NixieTube.cpp
+++ NixieTube/NixieTube.cpp
@@ -1,18 +1,23 @@
 /*
  * NixieTube.cpp
- *
- * Library for Nixie (Tube) Module for Arduino,
- * developed by Yan Zeyuan @ http://nixieclock.org
- *
  * Created on: 2012-5-4
- * Author: Weihong Guan (aGuegu) @ http://aguegu.net
  *
- * Host at https://github.com/aguegu/nixie-tube
+ * Library for Nixie (Tube) Module for Arduino
+ * Host: https://github.com/aguegu/nixie-tube
  *
  * licensed under Attribution-NonCommercial 3.0 Unported (CC BY-NC 3.0)
  * http://creativecommons.org/licenses/by-nc/3.0/
  *
+ * Hardware Designer: Yan Zeyuan
+ * Blog: http://nixieclock.org
+ * Email: yanzeyuan@163.com
+ *
+ * Library Author: Weihong Guan (aGuegu)
+ * Blog: http://aguegu.net
+ * Email: weihong.guan@gmail.com
+ *
  */
+
 #include "NixieTube.h"
 
 NixieTube::NixieTube(byte section_count)

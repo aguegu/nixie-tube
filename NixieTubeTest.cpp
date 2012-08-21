656dacb709371a0db75b540f8416cca055655459
Weihong Guan <weihong.guan@gmail.com>
2012-08-21 18:19:05 +0800

    add copyright info to sample

--- NixieTubeTest.cpp
+++ NixieTubeTest.cpp
@@ -1,5 +1,16 @@
 /* Basic sample code for nixie-tube module for Arduino
  *
+ * licensed under Attribution-NonCommercial 3.0 Unported (CC BY-NC 3.0)
+ * http://creativecommons.org/licenses/by-nc/3.0/
+ *
+ * Hardware Designer: Yan Zeyuan
+ * Blog: http://nixieclock.org
+ * Email: yanzeyuan@163.com
+ *
+ * Library Author: Weihong Guan (aGuegu)
+ * Blog: http://aguegu.net
+ * Email: weihong.guan@gmail.com
+ *
  * Pin Layout, right-side view, input from pins(male)
  *
  *	            /-------------\

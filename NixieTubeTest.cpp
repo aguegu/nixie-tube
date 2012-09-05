58f8adcfefd036f80275e158cc563d304743c1dc
Weihong Guan <weihong.guan@gmail.com>
2012-09-05 11:25:41 +0800

    continue working on digital tube display class in ubuntu

--- NixieTubeTest.cpp
+++ NixieTubeTest.cpp
@@ -29,42 +29,27 @@
  */
 
 #include "NixieTubeTest.h"
-#include "NixieTube.h"
+#include "DigitalTubeDisplay.h"
 
-
-#define COUNT	8
-// define how many modules in serial
-
-NixieTube tube(11, 12, 13, 10, COUNT);
-// pin_ds, pin_st. pin_sh, pin_oe(pwm pin is preferred), COUNT
+//DigitalTubeDisplay dtd(6, 7, 8, 5, A5, A3, 10, 12, A2, A4, 9, 11);
+DigitalTubeDisplay dtd;//(2, 11, 12, 9, 3, 13, 10, 6, 5, 4, 8, 7);
 
 void setup()
 {
 
-	for(byte i=0; i<COUNT; i++)
-	{
-		tube.setBackgroundColor(i, (Color)(i%8));	// set different background color for each module
-//		tube.setColon(i, (Colon)(i%4));
-//		tube.setNumber(i, i);
-	}
-
-	tube.setBrightness(0xff);	// brightness control, 0x00(off)-0xff
-
-	tube.display();
 }
 
 void loop()
 {
-	static byte h = 0, m = 0, s = 0, cs = 0;
+	static byte s = 0, m=0;
+	dtd.printf("%2d.%02d", m, s);
 
-	tube.printf("%02d:%02d:%02d'%02d", h, m, s, cs);
+	dtd.display(0x10);
 
-	tube.display();
-	delay(10);
-	cs++;
-	if (cs == 100) {cs=0; s++;}
-	if (s == 60) {s=0; m++;}
-	if (m == 60) {m=0; h++;}
-	if (h == 24) {h=0;}
-
+	s++;
+	if (s==100)
+	{
+		s = 0;
+		m++;
+	}
 }

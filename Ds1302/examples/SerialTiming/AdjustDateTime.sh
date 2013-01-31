2a4ce3918704a1d05620f569c5ecf4da9c4d113f
Weihong.Guan <weihong.Guan@gmail.com>
2013-01-31 13:54:33 +0800

    change AdjustDateTime.sh from symbolic link to hard link

--- Ds1302/examples/SerialTiming/AdjustDateTime.sh
+++ Ds1302/examples/SerialTiming/AdjustDateTime.sh
@@ -1,1 +1,20 @@
-/home/agu/workspace/Arduino/SerialTiming/AdjustDateTime.sh
+#!/bin/bash
+# filename: SerialTiming.sh
+# Description: To sync the RTC chip on Arduino, which needs correspondent program.
+# Author: Weihong Guan (@aGuegu)
+# blog: http://aguegu.net
+
+port=${1:-"/dev/ttyACM0"}
+baud=${2:-"9600"}
+
+stty -F $port $baud cs8 -cstopb -parenb cread clocal
+exec 3> $port
+sleep 2
+
+date +"[%T %D %w]" > $port 
+printf "mcu reply: \e[1;34m"
+sleep 0.5
+cat < $port
+echo -e "\e[0mdone."
+
+exec 3>&-
